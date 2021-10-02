224c0b5512056f05228cc5d6d5ac9f15094f572d
Krushil Naik <krushilnaik96@gmail.com>
2021-10-02 10:08:44 -0400

    Make contact with Watchmode API

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,34 +26,66 @@ let [inputMonth, inputDay, inputYear] = inputFields;
  *
  * Times this comment block used "the": 5
  */
-function requestMovie() {
+async function requestMovie() {
 	/**
-	 * For lack of both urgency and a better option, keep this in the code
+	 * For lack of both urgency and a better option, keep these in the code
 	 */
-	const API_KEY = '1160f108';
+	const OMDB_API_KEY = '1160f108';
+	const OMDB_URL = 'http://www.omdbapi.com';
+
+	const WATCHMODE_API_KEY = 'lRXZDQg46UX6Rtc0TC32jZ3ryc427Mmh14ChY3TH';
+	const WATCHMODE_URL = 'https://api.watchmode.com';
+
+	/**
+	 * Predefine these variables for usage with API calls
+	 * Recycling is good
+	 */
+	let response, data;
 
 	movieContainer.classList.add('loading');
 
-	// const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=the&y=${inputYear.value}&type=movie`;
+	// const OMDB_SEARCH = `${OMDB_URL}/?apikey=${OMDB_API_KEY}&t=the&y=${inputYear.value}&type=movie`;
+	const OMDB_SEARCH = '/sample/1996.json';
 
-	const URL = '/sample/1996.json';
+	response = await fetch(OMDB_SEARCH); // `${OMDB_URL}/?apikey=${OMDB_API_KEY}&t=the&y=${inputYear.value}&type=movie`
+	data = await response.json();
 
-	fetch(URL)
-		.then((response) => response.json())
-		.then((data) => {
-			const { Released, Title, Runtime, Poster, Ratings, Plot, Actors } = data;
+	const { Released, Title, Runtime, Poster, Ratings, Plot, Actors, imdbID } = data;
 
-			movieContainer.querySelector('img').src = Poster;
-			movieContainer.querySelector('#movie-title').innerHTML = Title;
-			movieContainer.querySelector('#release-date p').innerHTML = Released;
-			movieContainer.querySelector('#rating p').innerHTML = Ratings[0]['Value'];
-			movieContainer.querySelector('#runtime p').innerHTML = Runtime;
-			movieContainer.querySelector('#starring p').innerHTML = Actors;
-			movieContainer.querySelector('#plot').innerHTML = Plot;
+	movieContainer.querySelector('img').src = Poster;
+	movieContainer.querySelector('#movie-title').innerHTML = Title;
+	movieContainer.querySelector('#release-date p').innerHTML = Released;
+	movieContainer.querySelector('#rating p').innerHTML = Ratings[0]['Value'];
+	movieContainer.querySelector('#runtime p').innerHTML = Runtime;
+	movieContainer.querySelector('#starring p').innerHTML = Actors;
+	movieContainer.querySelector('#plot').innerHTML = Plot;
 
-			// this is only for testing the loader animation
-			movieContainer.classList.remove('loading');
-		});
+	/**
+	 * Pipe the previously fetched IMDb ID into the Watchmode API
+	 * and get all services the given movie is still streaming on
+	 */
+	// const WATCHMODE_SEARCH = `${WATCHMODE_URL}/v1/search/?apiKey=${WATCHMODE_API_KEY}&search_field=imdb_id&search_value=${imdbID}`;
+
+	// response = await fetch(WATCHMODE_SEARCH);
+	// data = await response.json();
+
+	if (!data.statusCode) {
+		// const watchmodeID = data.title_results[0].id;
+
+		// const WATCHMODE_TITLE = `${WATCHMODE_URL}/v1/title/${watchmodeID}/sources/?apiKey=${WATCHMODE_API_KEY}&regions=US`;
+		const WATCHMODE_TITLE = '/sample/the-rock.json';
+
+		response = await fetch(WATCHMODE_TITLE);
+		data = await response.json();
+
+		// do something with the data
+
+		console.log(data);
+	} else {
+		console.log(`Error fetching ${WATCHMODE_SEARCH}`);
+	}
+
+	movieContainer.classList.remove('loading');
 }
 
 /**
